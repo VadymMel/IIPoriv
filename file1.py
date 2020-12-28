408aecd730147590fe64639c683e33ecc6a14c37
v.melnychenko <v.melnychenko@testportal.gov.ua>
2020-12-28 17:41:19 +0200

    28.12.2020

--- file1.py
+++ file1.py
@@ -1,9 +1,7 @@
-import docx
-import numpy as np
 from fuzzywuzzy import fuzz
 import docx
 
-KEY_COMPARE = ['Послідовність виконання та  зміст робіт кожного', 'Положення політики безпеки,  пов’язані ',
+KEY_COMPARE = ['зміст Послідовність виконання та   робіт кожного', 'Положення політики безпеки,  пов’язані ',
                'позначень, ідентифікації об’єктів']
 doc = docx.Document('kszi.docx')
 # # количество абзацев в документе
@@ -14,11 +12,14 @@ dlinadoc = (len(doc.paragraphs))
 z = 0
 
 for i in KEY_COMPARE:
-
+    z += 1
+    print(f'CRUG PROVERKI = {z}')
     for abzac in doc.paragraphs:
         x = fuzz.partial_ratio(abzac.text, i)
         if x >= 80:
-
             print(x)
             print(abzac.text)
+        elif x >= 60:
+            print(f'x = {x}')
+            print(f'abzac = {abzac.text}')
 
